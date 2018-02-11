f9de3c26d7c92c59f54de9538e402d9f6673249b
Trashboat <14063870+Trashboat-99@users.noreply.github.com>
2018-02-10 19:27:25 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -20,4 +20,16 @@ app.get('*', routes.notFound);
 
 
 // Listen on port 3000
-app.listen(process.env.PORT || 3000);
+//app.listen(process.env.PORT || 3000);
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8081, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
